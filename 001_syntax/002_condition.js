b6da8df16c8a679326428d4d44d498b3b545c742
yko-git <yko.iwata@gmail.com>
2024-03-04 14:01:11 +0900

    fix 002_condition.js getDay

--- 001_syntax/002_condition.js
+++ 001_syntax/002_condition.js
@@ -55,53 +55,8 @@ function getDay(day) {
     "Saturday",
   ];
 
-  // for 失敗
-  // for (let i = 0; i < days.length - 1; i++) {
-  //   if (day === days[i]) {
-  //     return console.log(days[i]);
-  //   }
-  // }
-
-  // if
-  // if (day === 0) {
-  //   return console.log(days[0]);
-  // } else if (day === 1) {
-  //   return console.log(days[1]);
-  // } else if (day === 2) {
-  //   return console.log(days[2]);
-  // } else if (day === 3) {
-  //   return console.log(days[3]);
-  // } else if (day === 4) {
-  //   return console.log(days[4]);
-  // } else if (day === 5) {
-  //   return console.log(days[5]);
-  // } else if (day === 6) {
-  //   return console.log(days[6]);
-  // }
-
-  // switch
-  switch (day) {
-    case 0:
-      console.log(days[0]);
-      break;
-    case 1:
-      console.log(days[1]);
-      break;
-    case 2:
-      console.log(days[2]);
-      break;
-    case 3:
-      console.log(days[3]);
-      break;
-    case 4:
-      console.log(days[4]);
-      break;
-    case 5:
-      console.log(days[5]);
-      break;
-    case 6:
-      console.log(days[6]);
-      break;
+  if (days[day]) {
+    return console.log(days[day]);
   }
 }
 
