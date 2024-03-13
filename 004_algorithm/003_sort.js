eeb5f6a3e7713e46f33ef9338bb1c9d05ba0fb8d
yko-git <yko.iwata@gmail.com>
2024-03-13 10:33:47 +0900

    add 003_sort bubbleSort

--- 004_algorithm/003_sort.js
+++ 004_algorithm/003_sort.js
@@ -8,8 +8,23 @@
  *    [5, 3, 2, 1] => [1, 2, 3, 5]
  */
 
-function bubbleSort (array) {
-  return array
+function bubbleSort(array) {
+  //配列の数だけループ
+  for (let i = 0; i < array.length - 1; i++) {
+    //ループのネスト 配列の右端の要素からスタート
+    for (let j = array.length - 1; j > i; j--) {
+      //配列の右端が左隣の要素より小さかったら
+      if (array[j] < array[j - 1]) {
+        //tmp変数に該当の要素を代入して残す
+        let tmp = array[j];
+        //該当の要素を比較した要素に移動
+        array[j] = array[j - 1];
+        //比較した要素をtmpに変更
+        array[j - 1] = tmp;
+      }
+    }
+  }
+  return array;
 }
 
 /**
@@ -22,8 +37,8 @@ function bubbleSort (array) {
  *    [5, 3, 2, 1] => [1, 2, 3, 5]
  */
 
-function insertSort (array) {
-  return array
+function insertSort(array) {
+  return array;
 }
 
 /**
@@ -36,12 +51,11 @@ function insertSort (array) {
  *    [5, 3, 2, 1] => [1, 2, 3, 5]
  */
 
-function mergeSort (arr) {
-  return arr
+function mergeSort(arr) {
+  return arr;
 }
 
-function merge(left, right) {
-}
+function merge(left, right) {}
 
 /**
  *  2.2.4 クイックソート
@@ -53,13 +67,13 @@ function merge(left, right) {
  *    [5, 3, 2, 1] => [1, 2, 3, 5]
  */
 
-function quickSort (a, start = 0, end = (a.length -1)) {
-  return a
-};
+function quickSort(a, start = 0, end = a.length - 1) {
+  return a;
+}
 
 module.exports = {
   bubbleSort,
   insertSort,
   mergeSort,
-  quickSort
-}
+  quickSort,
+};
