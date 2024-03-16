09d4a966abfccd68fe4f47a1260c56c6b8ccaae5
yko-git <yko.iwata@gmail.com>
2024-03-16 10:59:07 +0900

    fix 002_medium removeVowels (論理演算子を変更)

--- 003_practice/002_medium.js
+++ 003_practice/002_medium.js
@@ -37,10 +37,10 @@ function removeVowels(str) {
 
   for (let i = 0; i < str.length; i++) {
     if (
-      str[i] !== "a" ||
-      str[i] !== "e" ||
-      str[i] !== "i" ||
-      str[i] !== "o" ||
+      str[i] !== "a" &&
+      str[i] !== "e" &&
+      str[i] !== "i" &&
+      str[i] !== "o" &&
       str[i] !== "u"
     ) {
       newStr += str[i];
