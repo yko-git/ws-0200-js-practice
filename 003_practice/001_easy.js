ea3c39eb89f496ce28d94eb0d92e7592b2544999
yko-git <yko.iwata@gmail.com>
2024-03-16 10:47:53 +0900

    fix 001_easy concat (スプレッド構文で書き直し)

--- 003_practice/001_easy.js
+++ 003_practice/001_easy.js
@@ -133,14 +133,7 @@ function average(array) {
  */
 
 function concat(a, b) {
-  let result = [];
-  for (let i = 0; i <= a.length - 1; i++) {
-    result.push(a[i]);
-  }
-  for (let j = 0; j <= b.length - 1; j++) {
-    result.push(b[j]);
-  }
-  return result;
+  return [...a, ...b];
 }
 
 /**
