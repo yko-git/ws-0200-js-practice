c674969dcdeb668d5b5290226b5cabb75f8b1588
yko-git <yko.iwata@gmail.com>
2024-08-24 07:02:53 +0900

    003_loop.js の復習（review-24_8）

--- 001_syntax/003_loop.js
+++ 001_syntax/003_loop.js
@@ -3,27 +3,47 @@
  *
  */
 
-function printOneToTen() {}
+function printOneToTen() {
+  for (let i = 0; i < 11; i++) {
+    console.log(i);
+  }
+}
 
 /**
  *  3.2 0~10までの数字を数字の大きい順に表示するメソッドを実装してください
  * */
 
-function printOneToTenDesc() {}
+function printOneToTenDesc() {
+  for (let i = 10; i >= 0; i--) {
+    console.log(i);
+  }
+}
 
 /**
  *  3.3 while文を使って0~10までの数字を小さい順番に表示するメソッドを実装してください
  *
  */
 
-function printOneToTenWhile() {}
+function printOneToTenWhile() {
+  let count = 0;
+  while (count < 11) {
+    console.log(count);
+    count++;
+  }
+}
 
 /**
  *  3.4 while文を使って0~10までの数字を大きい順番に表示するメソッドを実装してください
  *
  */
 
-function printOneToTenWhileDesc() {}
+function printOneToTenWhileDesc() {
+  let count = 10;
+  while (count >= 0) {
+    console.log(count);
+    count--;
+  }
+}
 
 module.exports = {
   printOneToTen,
