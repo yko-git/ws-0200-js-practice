8780bad43b17442948a8524527e2e069a37be067
yko-git <yko.iwata@gmail.com>
2024-08-25 07:03:23 +0900

    004_string.js の復習（review-24_8）

--- 001_syntax/004_string.js
+++ 001_syntax/004_string.js
@@ -3,28 +3,36 @@
  *
  */
 
-function length(str) {}
+function length(str) {
+  return str.length;
+}
 
 /**
  *  4.2 文字列のながさが偶数の場合にtrueを返却するメソッドを実装してください
  *
  */
 
-function lengthIsEven(str) {}
+function lengthIsEven(str) {
+  return str.length % 2 == 0 ?? true;
+}
 
 /**
  *  4.3 文字列の先頭一文字目を返却するメソッドを実装してください
  *
  */
 
-function firstChar(str) {}
+function firstChar(str) {
+  return str.charAt(0);
+}
 
 /**
  *  4.4 文字列の末尾一文字目を返却するメソッドを実装してください
  *
  */
 
-function lastChar(str) {}
+function lastChar(str) {
+  return str.charAt(str.length - 1);
+}
 
 /**
  *  4.5 文字列と二つの数字a, bを渡すとa文字目から, b文字目まで
@@ -32,7 +40,9 @@ function lastChar(str) {}
  *
  */
 
-function substring(str, a, b) {}
+function substring(str, a, b) {
+  return str.substring(a - 1, b);
+}
 
 /**
  *  4.6 引数に与えられた二つの引数のうち、一つ目の引数の文字列に二つ目の引数の文字列が
@@ -45,7 +55,9 @@ function substring(str, a, b) {}
  *      "hogegeho", "fugafuga" => false
  * */
 
-function isInclude(a, b) {}
+function isInclude(a, b) {
+  return a.includes(b);
+}
 
 /**
  *  4.7 引数で渡された文字列を一文字ずつ表示するメソッドを実装してください
@@ -62,7 +74,11 @@ function isInclude(a, b) {}
  *
  */
 
-function printByChar(str) {}
+function printByChar(str) {
+  for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
+  }
+}
 
 module.exports = {
   length,
