883b2820aacfeb2e1423f935add7b3917bb601f5
yko-git <yko.iwata@gmail.com>
2024-08-26 12:08:32 +0900

    005_array.js の復習（review-24_8）

--- 001_syntax/005_array.js
+++ 001_syntax/005_array.js
@@ -3,7 +3,11 @@
  *
  */
 
-function printArray(array) {}
+function printArray(array) {
+  array.forEach((element) => {
+    console.log(element);
+  });
+}
 
 /**
  *  5.2 関数内で全ての曜日を配列として宣言して返却するメソッドを実装してください
@@ -20,7 +24,17 @@ function printArray(array) {}
  *      ]
  *
  */
-function getDays() {}
+function getDays() {
+  return [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+}
 
 /**
  *  5.3 配列に特定の数があった場合にtrue、そうでない場合にfalseを返す関数を実装してください
@@ -32,7 +46,9 @@ function getDays() {}
  *
  */
 
-function findNum(array, num) {}
+function findNum(array, num) {
+  return array.includes(num);
+}
 
 /**
  *  5.4 配列に重複した要素があれば、true、そうでなければfalseを返す関数を実装してください
@@ -44,7 +60,13 @@ function findNum(array, num) {}
  *
  */
 
-function isDuplicate(array) {}
+function isDuplicate(array) {
+  const duplicate = array.filter((val, i, arr) => {
+    return arr.indexOf(val) != i;
+    // 元配列arrから要素valのindexと配列のindexが合致していない要素を新しく配列に格納
+  });
+  return duplicate.length >= 1 ?? true;
+}
 
 module.exports = {
   printArray,
