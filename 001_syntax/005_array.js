0d4804e9ca8a94357ad2cbbd019c0420350161b3
yko-git <yko.iwata@gmail.com>
2024-11-16 11:46:18 +0900

    001_syntax/005_array.js の復習（review-24_11）

--- 001_syntax/005_array.js
+++ 001_syntax/005_array.js
@@ -3,7 +3,11 @@
  *
  */
 
-function printArray(array) {}
+function printArray(array) {
+  array.forEach((value) => {
+    console.log(value);
+  });
+}
 
 /**
  *  5.2 関数内で全ての曜日を配列として宣言して返却するメソッドを実装してください
@@ -20,7 +24,18 @@ function printArray(array) {}
  *      ]
  *
  */
-function getDays() {}
+function getDays() {
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  return days;
+}
 
 /**
  *  5.3 配列に特定の数があった場合にtrue、そうでない場合にfalseを返す関数を実装してください
@@ -32,7 +47,9 @@ function getDays() {}
  *
  */
 
-function findNum(array, num) {}
+function findNum(array, num) {
+  return array.includes(num);
+}
 
 /**
  *  5.4 配列に重複した要素があれば、true、そうでなければfalseを返す関数を実装してください
@@ -43,8 +60,20 @@ function findNum(array, num) {}
  *    [] => false
  *
  */
+function isDuplicate(array) {
+  let duplicateObj = {};
 
-function isDuplicate(array) {}
+  for (let i = 0; i < array.length; i++) {
+    let target = array[i];
+    duplicateObj[target] = duplicateObj[target] ? duplicateObj[target] + 1 : 1;
+  }
+
+  const booleanArray = Object.values(duplicateObj).map((value) => {
+    return value >= 2;
+  });
+
+  return booleanArray.includes(true);
+}
 
 module.exports = {
   printArray,
