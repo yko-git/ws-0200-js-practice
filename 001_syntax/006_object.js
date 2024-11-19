7c6b6ebf137c41fc85f381236e22f9d34c874955
yko-git <yko.iwata@gmail.com>
2024-11-19 16:31:07 +0900

    001_syntax/006_object.js の復習（review-24_11）

--- 001_syntax/006_object.js
+++ 001_syntax/006_object.js
@@ -7,7 +7,13 @@
  *
  */
 
-function getPersonObject() {}
+function getPersonObject() {
+  return {
+    name: "Bob",
+    age: 32,
+    gender: "male",
+  };
+}
 
 /**
  *  6.2 下記データAが引数で与えられた場合にデータBに書き換える関数を実装してください。
@@ -23,7 +29,12 @@ function getPersonObject() {}
  *
  */
 
-function mutateObject(person) {}
+function mutateObject(person) {
+  person.name = "Mary";
+  person.age = 37;
+  person.gender = "female";
+  return person;
+}
 
 /**
  *  6.3 下記引数で渡される配列にランダムな1 ~10の数字を割り振り、オブジェクトとして返す
@@ -46,7 +57,12 @@ function mutateObject(person) {}
  *
  */
 
-function assignNumber(persons) {}
+function assignNumber(persons) {
+  return persons.reduce((acc, current) => {
+    const randomNum = 1 + Math.floor(Math.random() * 10);
+    return { ...acc, [current]: randomNum };
+  }, {});
+}
 
 /**
  *  6.4 配列に重複した要素があれば、true、そうでなければfalseを返す関数を実装してください
@@ -59,7 +75,18 @@ function assignNumber(persons) {}
  *
  */
 
-function isDuplicate(array) {}
+function isDuplicate(array) {
+  let obj = {};
+
+  for (let i = 0; i < array.length; i++) {
+    const value = array[i];
+    if (obj[value]) {
+      return true;
+    }
+    obj[value] = true;
+  }
+  return false;
+}
 
 module.exports = {
   getPersonObject,
