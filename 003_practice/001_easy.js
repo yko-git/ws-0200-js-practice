d359b3d4ebf899e854ac90cbcecb974e3db00188
yko-git <yko.iwata@gmail.com>
2024-12-11 10:47:09 +0900

    003_practice/001_easy.js の復習（review-24_11）

--- 003_practice/001_easy.js
+++ 003_practice/001_easy.js
@@ -256,7 +256,19 @@ function filter(array, num) {
  *    ...
  */
 
-function fizzBuzz() {}
+function fizzBuzz() {
+  for (let i = 1; i < 100 + 1; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log(`${i} FizzBuzz`);
+    } else if (i % 3 === 0) {
+      console.log(`${i} Fizz`);
+    } else if (i % 5 === 0) {
+      console.log(`${i} Buzz`);
+    } else {
+      console.log(i);
+    }
+  }
+}
 
 module.exports = {
   length,
