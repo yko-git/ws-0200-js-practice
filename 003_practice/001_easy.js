1d6093925b5a85fb9210601aafd5cf3bfe7d6126
yko-git <yko.iwata@gmail.com>
2025-04-20 06:25:38 +0900

    003_practice/001_easy.js の復習（review-25_3）

--- 003_practice/001_easy.js
+++ 003_practice/001_easy.js
@@ -160,7 +160,12 @@ function size(array) {
  *
  */
 
-function minMax(array) {}
+function minMax(array) {
+  if (!array.length) {
+    return "";
+  }
+  return console.log(`max: ${Math.max(...array)}, min: ${Math.min(...array)}`);
+}
 
 /**
  *  連番
