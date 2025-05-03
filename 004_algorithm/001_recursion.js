9537ee0bbdc93a87bd155b457f0118674c93cfcd
yko-git <yko.iwata@gmail.com>
2025-05-04 05:44:11 +0900

    004_algorithm/001_recursion.js の復習（review-25_3）

--- 004_algorithm/001_recursion.js
+++ 004_algorithm/001_recursion.js
@@ -13,8 +13,7 @@ function sumSequence(n, sum = 0) {
   if (n === 0) {
     return sum;
   }
-  sum += n;
-  return sumSequence(n - 1, sum);
+  return sumSequence(n - 1, (sum += n));
 }
 
 /**
@@ -27,13 +26,7 @@ function sumSequence(n, sum = 0) {
  *    input: 10 => [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
  */
 
-function fibonacci(num, result = [1, 1]) {
-  if (result.length === num) {
-    return result;
-  }
-  result.push(result[result.length - 1] + result[result.length - 2]);
-  return fibonacci(num, result);
-}
+function fibonacci(num, result = [1, 1]) {}
 
 /**
  *  2.4.2 ディレクトリに含まれるファイルサイズの合計
@@ -87,15 +80,7 @@ function fibonacci(num, result = [1, 1]) {
  *    => 38
  */
 
-function fileSize(node, sum = 0) {
-  if (!node.children) {
-    return sum + node.size;
-  }
-  for (let value of node.children) {
-    sum += fileSize(value);
-  }
-  return sum;
-}
+function fileSize(node, sum = 0) {}
 
 module.exports = {
   sumSequence,
