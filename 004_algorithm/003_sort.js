57aec31457af8b251ba520f6f8c6a4a3b450d2ea
yko-git <yko.iwata@gmail.com>
2025-05-18 07:23:37 +0900

    004_algorithm/003_sort.js の復習（review-25_3）

--- 004_algorithm/003_sort.js
+++ 004_algorithm/003_sort.js
@@ -11,10 +11,10 @@
 function bubbleSort(array) {
   let temp;
   for (let i = 0; i < array.length; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[i] > array[j]) {
-        temp = array[i];
-        array[i] = array[j];
+    for (let j = 0; j < array.length; j++) {
+      temp = array[j + 1];
+      if (array[j] > temp) {
+        array[j + 1] = array[j];
         array[j] = temp;
       }
     }
@@ -33,18 +33,6 @@ function bubbleSort(array) {
  */
 
 function insertSort(array) {
-  let temp, j; // 挿入する値と比較に使うインデックス用の変数
-  for (let i = 1; i < array.length; i++) {
-    // 配列の2番目の要素（index 1）からループ
-    temp = array[i]; // 挿入対象の値を一時的に保存
-    j = i - 1; // i の1つ前の要素を j に設定
-    while (j >= 0 && array[j] > temp) {
-      // j が 0 以上で、array[j] が temp より大きい間
-      array[j + 1] = array[j]; // 右に1つ移動
-      j--; // j をデクリメント（左に移動）
-    }
-    array[j + 1] = temp; // 空いた位置に temp を挿入
-  }
   return array;
 }
 
@@ -58,26 +46,9 @@ function insertSort(array) {
  *    [5, 3, 2, 1] => [1, 2, 3, 5]
  */
 
-function mergeSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
-  let left = mergeSort(arr.slice(0, Math.floor(arr.length / 2)));
-  let right = mergeSort(arr.slice(Math.floor(arr.length / 2)));
-  return merge(left, right);
-}
+function mergeSort(arr) {}
 
-function merge(left, right) {
-  const resultArray = [];
-  while (left.length > 0 && right.length > 0) {
-    if (left[0] > right[0]) {
-      resultArray.push(right.shift());
-    } else {
-      resultArray.push(left.shift());
-    }
-  }
-  return resultArray.concat(left, right);
-}
+function merge(left, right) {}
 
 /**
  *  2.2.4 クイックソート
