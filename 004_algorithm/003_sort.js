2397414cd95ddd758b7da087cdcf28460c32896b
yko-git <yko.iwata@gmail.com>
2025-05-20 06:39:43 +0900

    004_algorithm/003_sort.js の復習（review-25_3）

--- 004_algorithm/003_sort.js
+++ 004_algorithm/003_sort.js
@@ -32,6 +32,15 @@ function bubbleSort(array) {
  */
 
 function insertSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    let temp = array[i];
+    let j = i - 1;
+    while (j >= 0 && temp < array[j]) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = temp;
+  }
   return array;
 }
 
