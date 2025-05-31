968233a3c7a89938297583531432ca1cf9f1c996
yko-git <yko.iwata@gmail.com>
2025-06-01 07:09:59 +0900

    004_algorithm/003_sort.js の復習（review-25_3）

--- 004_algorithm/003_sort.js
+++ 004_algorithm/003_sort.js
@@ -86,8 +86,14 @@ function merge(left, right) {
  *    [5, 3, 2, 1] => [1, 2, 3, 5]
  */
 
-function quickSort(a, start = 0, end = a.length - 1) {
-  return a;
+function quickSort(a) {
+  if (a.length <= 1) {
+    return a;
+  }
+  const pivot = a[0];
+  const left = a.slice(1).filter((x) => x < pivot);
+  const right = a.slice(1).filter((x) => x >= pivot);
+  return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
 module.exports = {
