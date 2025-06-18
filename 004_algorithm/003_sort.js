2d7025fb1aa56cfc96d016a6b8d57d5b6a3b80cf
yko-git <yko.iwata@gmail.com>
2025-06-19 06:22:34 +0900

    004_algorithm/003_sort.js の復習（review-25_3）

--- 004_algorithm/003_sort.js
+++ 004_algorithm/003_sort.js
@@ -31,19 +31,7 @@ function bubbleSort(array) {
  *    [5, 3, 2, 1] => [1, 2, 3, 5]
  */
 
-function insertSort(array) {
-  let temp, j;
-  for (let i = 1; i < array.length; i++) {
-    temp = array[i];
-    j = i - 1;
-    while (j >= 0 && temp < array[j]) {
-      array[j + 1] = array[j];
-      j--;
-    }
-    array[j + 1] = temp;
-  }
-  return array;
-}
+function insertSort(array) {}
 
 /**
  *  2.2.3 マージソート
@@ -55,26 +43,9 @@ function insertSort(array) {
  *    [5, 3, 2, 1] => [1, 2, 3, 5]
  */
 
-function mergeSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
-  let left = mergeSort(arr.slice(0, Math.floor(arr.length / 2)));
-  let right = mergeSort(arr.slice(Math.floor(arr.length / 2)));
-  return merge(left, right);
-}
+function mergeSort(arr) {}
 
-function merge(left, right) {
-  const newArray = [];
-  while (left.length > 0 && right.length > 0) {
-    if (left[0] > right[0]) {
-      newArray.push(right.shift());
-    } else {
-      newArray.push(left.shift());
-    }
-  }
-  return newArray.concat(left, right);
-}
+function merge(left, right) {}
 
 /**
  *  2.2.4 クイックソート
@@ -86,15 +57,7 @@ function merge(left, right) {
  *    [5, 3, 2, 1] => [1, 2, 3, 5]
  */
 
-function quickSort(a) {
-  if (a.length <= 1) {
-    return a;
-  }
-  const pivot = a[0];
-  const left = a.slice(1).filter((x) => x < pivot);
-  const right = a.slice(1).filter((x) => x >= pivot);
-  return [...quickSort(left), pivot, ...quickSort(right)];
-}
+function quickSort(a) {}
 
 module.exports = {
   bubbleSort,
