e069a118df6b2fe0f1c0f2400364f2e4f42896a9
yko-git <yko.iwata@gmail.com>
2025-07-14 06:30:34 +0900

    004_algorithm/002_search.js の復習（review-25_3）

--- 004_algorithm/002_search.js
+++ 004_algorithm/002_search.js
@@ -29,7 +29,21 @@ function linearSearch(array, target) {
  *    [1, 2, 3, 4] 5 => -1
  */
 
-function binarySearch(array, target, low = 0, high = array.length - 1) {}
+function binarySearch(array, target, low = 0, high = array.length - 1) {
+  if (low > high) {
+    return -1;
+  }
+  const mid = Math.floor((low + high) / 2);
+  if (array[mid] === target) {
+    return mid;
+  }
+  if (array[mid] < target) {
+    return binarySearch(array, target, mid + 1, high);
+  }
+  if (array[mid] > target) {
+    return binarySearch(array, target, low, mid - 1);
+  }
+}
 
 module.exports = {
   linearSearch,
