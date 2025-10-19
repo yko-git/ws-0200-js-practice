5a9fd26470971a17c6e498799ee1f430abdb7202
yko-git <yko.iwata@gmail.com>
2025-10-19 11:17:10 +0900

    001_syntax/002_condition.js の復習（review-25_10）

--- 001_syntax/002_condition.js
+++ 001_syntax/002_condition.js
@@ -40,7 +40,22 @@ function greeding(hour) {
  *
  */
 
-function getDay(day) {}
+function getDay(day) {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  days.forEach((d, index) => {
+    if (index === day) {
+      console.log(d);
+    }
+  });
+}
 
 module.exports = {
   printMessageByAge,
