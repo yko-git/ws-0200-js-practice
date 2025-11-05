0315a612dd20c764bd3fcffd93103f00a282839c
yko-git <yko.iwata@gmail.com>
2025-11-05 09:51:24 +0900

    001_syntax/006_object.js の復習（review-25_10）

--- 001_syntax/006_object.js
+++ 001_syntax/006_object.js
@@ -25,7 +25,9 @@ function getPersonObject() {
  *
  */
 
-function mutateObject(person) {}
+function mutateObject(person) {
+  return { name: "Mary", age: 37, gender: "female" };
+}
 
 /**
  *  6.3 下記引数で渡される配列にランダムな1 ~10の数字を割り振り、オブジェクトとして返す
