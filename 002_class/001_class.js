b9f22ed51bc3145053b340a637cf47cd3a9edeca
yko-git <yko.iwata@gmail.com>
2025-11-10 06:49:30 +0900

    002_class/001_class.js の復習（review-25_10）

--- 002_class/001_class.js
+++ 002_class/001_class.js
@@ -21,12 +21,25 @@
  */
 
 class Person {
-  constructor(name, age, bio) {}
+  constructor(name = "", age = 0, bio = "") {
+    this.name = name;
+    this.age = age;
+    this.bio = bio;
+  }
   sayHi() {
     console.log("Hi!!");
   }
   static describe() {
     console.log("This is a Person class");
+  }
+  get isUnderage() {
+    if (this.age < 20) {
+      return true;
+    }
+    return false;
+  }
+  toString() {
+    return `name: ${this.name}, age: ${this.age}, bio: ${this.bio}`;
   }
 }
 
