7132b92cdf21ee66018dec67bd9110676fb992ef
yko-git <yko.iwata@gmail.com>
2026-01-22 09:13:30 +0900

    003_practice/002_medium.js の復習（review-25_12）

--- 003_practice/002_medium.js
+++ 003_practice/002_medium.js
@@ -94,7 +94,11 @@ function isPalindrome(str) {
  *    11 => True
  *
  */
-function isPrime(num) {}
+function isPrime(num) {
+  if (num === 1) return false;
+  if (num === 2 || num === 3) return true;
+  return num % 2 !== 0 && num % 3 !== 0 && num % 5 !== 0 && num % 7 !== 0;
+}
 
 /**
  *  配列の4と次の数字を抜いた合計
