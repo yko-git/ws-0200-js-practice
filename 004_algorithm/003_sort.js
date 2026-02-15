fdca116467db7d30fa2c79d4b56c9f5b644ec1b3
yko-git <yko.iwata@gmail.com>
2026-02-16 07:02:39 +0900

    004_algorithm/003_sort.js の復習（review-25_12）

--- 004_algorithm/003_sort.js
+++ 004_algorithm/003_sort.js
@@ -9,9 +9,9 @@
  */
 function bubbleSort(array) {
   for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length; j++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
       let temp = array[j];
-      if (array[j + 1] < temp) {
+      if (temp > array[j + 1]) {
         array[j] = array[j + 1];
         array[j + 1] = temp;
       }
@@ -30,19 +30,7 @@ function bubbleSort(array) {
  *    [5, 3, 2, 1] => [1, 2, 3, 5]
  */
 
-function insertSort(array) {
-  let j, temp;
-  for (let i = 1; i < array.length; i++) {
-    temp = array[i];
-    j = i - 1;
-    while (j >= 0 && array[j] > temp) {
-      array[j + 1] = array[j];
-      j--;
-    }
-    array[j + 1] = temp;
-  }
-  return array;
-}
+function insertSort(array) {}
 
 /**
  *  2.2.3 マージソート
@@ -53,24 +41,9 @@ function insertSort(array) {
  *    [1, 3, 2, 4, 5] => [1, 2, 3, 4, 5]
  *    [5, 3, 2, 1] => [1, 2, 3, 5]
  */
-function mergeSort(arr) {
-  if (arr.length <= 1) return arr;
-  const left = mergeSort(arr.slice(0, Math.floor(arr.length / 2)));
-  const right = mergeSort(arr.slice(Math.floor(arr.length / 2)));
-  return merge(left, right);
-}
+function mergeSort(arr) {}
 
-function merge(left, right) {
-  const newArray = [];
-  while (left.length >= 1 && right.length >= 1) {
-    if (left[0] < right[0]) {
-      newArray.push(left.shift());
-    } else {
-      newArray.push(right.shift());
-    }
-  }
-  return newArray.concat(left, right);
-}
+function merge(left, right) {}
 
 /**
  *  2.2.4 クイックソート
@@ -82,21 +55,7 @@ function merge(left, right) {
  *    [5, 3, 2, 1] => [1, 2, 3, 5]
  */
 
-function quickSort(a, start = 0, end = a.length - 1) {
-  if (a.length <= 1) return a;
-  const pivot = a[0];
-  const left = [];
-  const right = [];
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] < pivot) {
-      left.push(a[i]);
-    }
-    if (a[i] > pivot) {
-      right.push(a[i]);
-    }
-  }
-  return [...quickSort(left), pivot, ...quickSort(right)];
-}
+function quickSort(a, start = 0, end = a.length - 1) {}
 
 module.exports = {
   bubbleSort,
