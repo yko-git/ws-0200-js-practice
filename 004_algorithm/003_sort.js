5dc84605e8dc96f00d7487f9469fda92de7e81aa
yko-git <yko.iwata@gmail.com>
2026-02-18 06:17:43 +0900

    004_algorithm/003_sort.js の復習（review-25_12）

--- 004_algorithm/003_sort.js
+++ 004_algorithm/003_sort.js
@@ -67,7 +67,14 @@ function merge(left, right) {}
  *    [5, 3, 2, 1] => [1, 2, 3, 5]
  */
 
-function quickSort(a, start = 0, end = a.length - 1) {}
+function quickSort(a, start = 0, end = a.length - 1) {
+  if (a.length <= 1) return a;
+  const pivot = a[0];
+  const left = a.filter((x) => x < pivot);
+  const mid = a.filter((x) => x === pivot);
+  const right = a.filter((x) => x > pivot);
+  return [...quickSort(left), ...mid, ...quickSort(right)];
+}
 
 module.exports = {
   bubbleSort,
