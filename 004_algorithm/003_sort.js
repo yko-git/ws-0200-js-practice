1fc9214fbfe792917c3ee34d5afa77f0182ac7f6
yko-git <yko.iwata@gmail.com>
2026-02-22 11:47:37 +0900

    004_algorithm/003_sort.js の復習（review-25_12）

--- 004_algorithm/003_sort.js
+++ 004_algorithm/003_sort.js
@@ -36,10 +36,10 @@ function insertSort(array) {
     temp = array[i];
     j = i - 1;
     while ((j >= 0, temp < array[j])) {
-      array[j] = temp;
+      array[j + 1] = array[j];
       j--;
     }
-    array[j + 1] = array[j + 1];
+    array[j + 1] = temp;
   }
   return array;
 }
