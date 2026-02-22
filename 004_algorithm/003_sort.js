4af643881770169ebcb1a0a80153ed306e7a918d
yko-git <yko.iwata@gmail.com>
2026-02-23 07:20:37 +0900

    004_algorithm/003_sort.js の復習（review-25_12）

--- 004_algorithm/003_sort.js
+++ 004_algorithm/003_sort.js
@@ -53,9 +53,24 @@ function insertSort(array) {
  *    [1, 3, 2, 4, 5] => [1, 2, 3, 4, 5]
  *    [5, 3, 2, 1] => [1, 2, 3, 5]
  */
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  const left = mergeSort(arr.slice(0, Math.floor(arr.length / 2)));
+  const right = mergeSort(arr.slice(Math.floor(arr.length / 2)));
+  return merge(left, right);
+}
 
-function merge(left, right) {}
+function merge(left, right) {
+  const newArray = [];
+  while (left.length >= 1 && right.length >= 1) {
+    if (left[0] > right[0]) {
+      newArray.push(right.shift());
+    } else {
+      newArray.push(left.shift());
+    }
+  }
+  return newArray.concat(left, right);
+}
 
 /**
  *  2.2.4 クイックソート
