4446c607f27b693aa3de05cf6616478ffe4f5036
yko-git <yko.iwata@gmail.com>
2026-04-03 06:13:25 +0900

    001_syntax/005_array.js の復習（review-26_3）

--- 001_syntax/005_array.js
+++ 001_syntax/005_array.js
@@ -3,7 +3,11 @@
  *
  */
 
-function printArray(array) {}
+function printArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+}
 
 /**
  *  5.2 関数内で全ての曜日を配列として宣言して返却するメソッドを実装してください
