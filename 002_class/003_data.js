f06a329f50ecdba4501ee866bd441e76f3484df4
yko-git <yko.iwata@gmail.com>
2026-04-24 06:18:07 +0900

    002_class/003_data.js の復習（review-26_3）

--- 002_class/003_data.js
+++ 002_class/003_data.js
@@ -103,6 +103,12 @@ class List {
    */
   findIndex(target) {
     // TODO:
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i] === target) {
+        return i;
+      }
+    }
+    return -1;
   }
 
   /** リストの の中から要素に合致する数を取り除く
