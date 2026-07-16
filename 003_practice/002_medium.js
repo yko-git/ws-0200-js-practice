afc8671291fc6332ce24039d61e8496527729aed
yko-git <yko.iwata@gmail.com>
2026-07-17 06:28:20 +0900

    003_practice/002_medium.js の復習（review-26_7）

--- 003_practice/002_medium.js
+++ 003_practice/002_medium.js
@@ -52,7 +52,16 @@ function removeVowels(str) {
  *    'hogehoage',  'hoge' => 1
  *
  */
-function countStr(s1, s2) {}
+function countStr(s1, s2, count = 0) {
+  let index;
+
+  if (s1.includes(s2)) {
+    count++;
+    index = s1.indexOf(s2) + s2.length;
+    return countStr(s1.slice(index), s2, count);
+  }
+  return count;
+}
 
 /**
  *  引数に与えられたアルファベットの文字列が回文であること
