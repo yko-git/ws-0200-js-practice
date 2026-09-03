3cfbbd01ef93964b4b6c99c8d2bee1164bebc10f
yko-git <yko.iwata@gmail.com>
2026-09-03 09:20:52 +0900

    001_syntax/005_array.js の復習（review-26_9）

--- 001_syntax/005_array.js
+++ 001_syntax/005_array.js
@@ -63,7 +63,16 @@ function findNum(array, num) {
  *
  */
 
-function isDuplicate(array) {}
+function isDuplicate(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length - 1; j++) {
+      if (array[i] === array[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
 
 module.exports = {
   printArray,
