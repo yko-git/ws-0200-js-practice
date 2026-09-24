0b81b0cb7f01c21cb975ceec5ca43c98045d4a74
yko-git <yko.iwata@gmail.com>
2026-09-24 09:00:17 +0900

    002_class/003_data.js の復習（review-26_9）

--- 002_class/003_data.js
+++ 002_class/003_data.js
@@ -87,6 +87,11 @@ class List {
    */
   find(target) {
     // TODO:
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i] === target) {
+        return this.data[i];
+      }
+    }
   }
 
   /** リストの の中から引数に合致する値のindexを取得する。見つからない場合は-1を返す
